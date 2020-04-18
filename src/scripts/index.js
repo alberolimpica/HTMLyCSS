9ceaf1410e08dfde21e562a0f26fe6cfb40cd8d8
sara <salma4@alumni.uv.es>
2020-04-18 21:02:03 +0200

    youtube mod

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,3 @@
 
 console.log ("Hello world")
 
-function menuResponsive() {
-    var x = document.getElementById("menuNav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  }
